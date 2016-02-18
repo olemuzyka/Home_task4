1c828dfc02f471a37515ed4a8d91bb1cdcf45798
olemuzyka <olemuzyka@gmail.com>
2016-02-18 13:25:54 +0200

    home task_4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,41 +4,41 @@ public class Main {
 
     public static void main(String[] args) {
         //TASK_1-------------------------------------------
-        int[][] arrayC = new int[3][3];
-        for (int i = 0; i < arrayC.length; i++) {
-            for (int j = 0; j < arrayC.length; j++) {
-                arrayC[i][j] = (int) (Math.random() * 100);
-                System.out.print(arrayC[i][j] + " ");
-            }
-            System.out.println(" ");
-        }
-        System.out.println(" ");
-        int[][] arrayD = new int[3][3];
-        for (int i = 0; i < arrayD.length; i++) {
-            for (int j = 0; j < arrayD.length; j++) {
-                arrayD[i][j] = (int) (Math.random() * 100);
-                System.out.print(arrayD[i][j] + " ");
-            }
-            System.out.println(" ");
-        }
-        System.out.println(" SUM ");
-        int[][] arrayX = new int[3][3];
-        for (int i = 0; i < arrayX.length; i++) {
-            for (int j = 0; j < arrayX.length; j++) {
-                arrayX[i][j] = arrayC[i][j] + arrayD[i][j];
-                System.out.print(arrayX[i][j] + " ");
-            }
-            System.out.println(" ");
-        }
-        System.out.println(" multiplication ");
-        int[][] arrayZ = new int[3][3];
-        for (int i = 0; i < arrayZ.length; i++) {
-            for (int j = 0; j < arrayZ.length; j++) {
-                arrayZ[i][j] += arrayC[i][j] * arrayD[j][i];
-                System.out.print(arrayZ[i][j] + " ");
-            }
-            System.out.println(" ");
-        }
+//        int[][] arrayC = new int[3][3];
+//        for (int i = 0; i < arrayC.length; i++) {
+//            for (int j = 0; j < arrayC.length; j++) {
+//                arrayC[i][j] = (int) (Math.random() * 100);
+//                System.out.print(arrayC[i][j] + " ");
+//            }
+//            System.out.println(" ");
+//        }
+//        System.out.println(" ");
+//        int[][] arrayD = new int[3][3];
+//        for (int i = 0; i < arrayD.length; i++) {
+//            for (int j = 0; j < arrayD.length; j++) {
+//                arrayD[i][j] = (int) (Math.random() * 100);
+//                System.out.print(arrayD[i][j] + " ");
+//            }
+//            System.out.println(" ");
+//        }
+//        System.out.println(" SUM ");
+//        int[][] arrayX = new int[3][3];
+//        for (int i = 0; i < arrayX.length; i++) {
+//            for (int j = 0; j < arrayX.length; j++) {
+//                arrayX[i][j] = arrayC[i][j] + arrayD[i][j];
+//                System.out.print(arrayX[i][j] + " ");
+//            }
+//            System.out.println(" ");
+//        }
+//        System.out.println(" multiplication ");
+//        int[][] arrayZ = new int[3][3];
+//        for (int i = 0; i < arrayZ.length; i++) {
+//            for (int j = 0; j < arrayZ.length; j++) {
+//                arrayZ[i][j] += arrayC[i][j] * arrayD[j][i];
+//                System.out.print(arrayZ[i][j] + " ");
+//            }
+//            System.out.println(" ");
+//        }
         //Task_2----------------------------------------
 //        int[][] array = new int[5][5];
 //        for (int i = 0; i < array.length; i++) {
@@ -57,27 +57,31 @@ public class Main {
 //
 //            }
 //        }
-            //TASK_3---------------------------------------------
-//        int[]arr = new int[40];
-//        for (int i = 0; i <arr.length ; i++) {
-//            arr[i]=(int)(Math.random()*201)+100;
-//           System.out.print(arr[i]+ " ");
-//        }
-//        int max = arr[0];
-//        int min = arr[0];
-//        for (int i = 0; i <arr.length ; i++) {
-//            if(max<arr[i]){
-//                max=arr[i];
-//            }
-//            if(min>arr[i]){
-//                min=arr[i];
-//            }
-//            System.out.println("MAX: " + max);
-//            System.out.println("MIN: " + min);
-//
-//        }
+        //TASK_3---------------------------------------------
+        int[] arr = new int[40];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 201) - 100;
+            System.out.print(arr[i] + " ");
+        }
+        int max = arr[0];
+                for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println("\nMAX: " + max);
+        System.out.println("MIN: " + min);
 
     }
 
 }
+
+
 
